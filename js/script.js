81ff1ef80e333048c374d8f7ec7b314a5942519a
caleighchapman <caleighechapman@gmail.com>
2026-03-25 17:43:08 -0500

    think i fixed button

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 console.log("script.js loaded");
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=kGZcEAGsKKQPd8QTlC8Whof9qrp3vJWO&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+const gifContainer = document.querySelector("#gif-container");
+const button = document.querySelector("#fetch-gif-btn");
+
 async function getCats() {
     const response = await fetch(endpoint);
     const data = await response.json();
     const images = data.data.map(gif => gif.images.original.url);
-    console.log(images);
-
-    const output = document.getElementById("cat-images");
-    output.textContext = images;
-
+    
+    gifContainer.innerHTML = "";
+    images.forEach(url => {
+        gifContainer.innerHTML += `<img src="${url}" class="col-3 mb-3">`;
+    });
+    
 }
 
-getCats();
-const button = document.getElementById("fetch-cat-button");
-
 button.addEventListener("click", function() {
-    getCats();
+    getCats();  
 });
